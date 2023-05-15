6d1e8fa84c5e45e262f3e4f76aaa4be1a881aadc
ckejo <jocic.andrija360@gmail.com>
2023-05-15 20:53:19 +0200

    vigenere cipher (almost working)

--- prog-projektni-vigenere-sifra/Form1.cs
+++ prog-projektni-vigenere-sifra/Form1.cs
@@ -10,37 +10,96 @@ namespace prog_projektni_vigenere_sifra
             InitializeComponent();
         }
 
+        public char[,] tabulaRecta = new char[26, 26]
+        {
+            {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
+            {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a'},
+            {'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b'},
+            {'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c'},
+            {'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd'},
+            {'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e'},
+            {'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f'},
+            {'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g'},
+            {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'},
+            {'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'},
+            {'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'},
+            {'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'},
+            {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'},
+            {'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'},
+            {'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'},
+            {'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'},
+            {'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'},
+            {'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'},
+            {'t', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's'},
+            {'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'},
+            {'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'},
+            {'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'},
+            {'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'},
+            {'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x'},
+            {'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'},
+        };
+
         private string GenerateKey()
         {
-            string input = tbInput.Text;
-            string keyword = tbKeyword.Text;
-            int x = input.Length;
-            string nKey = keyword;
+            string input = tbInput.Text.Trim();
+            string keyword = tbKeyword.Text.Trim();
+            StringBuilder nKeyword = new StringBuilder();
+            int iN = input.Length;
+            int kN = keyword.Length;
 
-            for (int i = 0; ; ++i)
+            int kNiniN = iN / kN;
+            int kNiniNMod = iN % kN;
+
+            // dodaje onoliko kljuca koliko se celih duzinski sadrzi u plaintextu
+            for (int i = 0; i < kNiniN; ++i)
             {
-                if (x == i) i = 0;
-                if (nKey.Length == input.Length) break;
-                nKey += keyword[i];
+                nKeyword.Append(keyword);
             }
 
-            return nKey;
+            // dodaje preostali deo
+            for (int i = 0; i < kNiniNMod; ++i)
+            {
+                nKeyword.Append(keyword[i]);
+            }
+
+            return nKeyword.ToString().ToLower();
         }
 
         private void bSolve_Click(object sender, EventArgs e)
         {
-            string input = tbInput.Text;
-            string keyword = GenerateKey();
-            StringBuilder nString = new StringBuilder();
+            string input = tbInput.Text.ToLower().Trim();
+            string nKeyword = GenerateKey();
+            StringBuilder cipher = new StringBuilder();
+            int len = input.Length;
 
-            for (int i = 0; i < input.Length; ++i)
+            for (int i = 0; i < len; ++i)
             {
-                int x = (input[i] + keyword[i]) % 26;
-                x += 'A';
-                nString.Append((char)x);
+                int vIndex = 0;
+                int kIndex = 0;
+
+                for (int v = 0; v < 26; ++v)
+                {
+                    if (input[i] == tabulaRecta[v, 0])
+                    {
+                        vIndex = v;
+                        break;
+                    }
+                }
+
+                for (int k = 0; k < 26; ++k)
+                {
+                    if (nKeyword[i] == tabulaRecta[0, k])
+                    {
+                        kIndex = k;
+                        break;
+                    }
+                }
+
+                cipher.Append(tabulaRecta[vIndex, kIndex]);
             }
 
-            tbResult.Text = nString.ToString();
+            tbResult.Text = cipher.ToString();
         }
     }
 }
